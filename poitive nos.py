4c7430e539c71760274973db15e4beae162020ba
Nikita-chodankar <nikichodan4@gmail.com>
2024-12-18 11:09:54 +0530

    positive nos

---
+++ poitive nos.py
@@ -0,0 +1,10 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+positive_1 = [num for num in list1 if num > 0]
+print("input: list=",list1 , "output:" , positive_1 )
+
+positive_2=[num for num in list2 if num >0]
+print("input: list2=", list2,"output:",positive_2)
+
+
+
